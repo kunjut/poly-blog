5e98f7566968d08272f347dcd416c7b386b587c1
Vadim Dimnich <v.dimnich@gmail.com>
2019-10-31 00:48:41 +0300

    other

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+ 
+  root 'posts#index'
+
+  resources :comments
+  resources :links
+  resources :posts
+  resources :images
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
